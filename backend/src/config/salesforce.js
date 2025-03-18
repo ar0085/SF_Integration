7eb86a168e791055e23c7b10c7779ff74cb75177
ar0085 <aruninani0199@gmail.com>
2025-03-18 22:45:48 +0530

    updated env variables

--- backend/src/config/salesforce.js
+++ backend/src/config/salesforce.js
@@ -10,8 +10,7 @@ module.exports = {
       loginUrl: SF_LOGIN_URL || "https://login.salesforce.com",
       clientId: SF_CONSUMER_KEY,
       clientSecret: SF_CONSUMER_SECRET,
-      redirectUri:
-        "https://useful-osprey-hideously.ngrok-free.app/api/auth/salesforce/callback",
+      redirectUri: SF_CALLBACK_URL,
     });
   },
 };
